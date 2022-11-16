fc04ab4546c7ce99803fe2fd62bef0137cc271be
Agnesss <114547235+nesgrc@users.noreply.github.com>
2022-11-16 21:20:51 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,12 +39,44 @@ window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== POPULAR SWIPER ===============*/
+let swiperPopular = new Swiper("popular__container", {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+    grabCursor: true,
 
+    pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {      
+      spaceBetween: 48,
+    },
+  },
+});
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
-
+let mixerFeatured = mixitup('.featured__content', {
+    selectors: {
+        target: '.featured__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
 /* Link active featured */ 
+const LinkFeatured = document.querySelectorAll('.featured__item')
+
+function activeFeatured(){
+    LinkFeatured.forEach(l=> l.classList.remove('active-featured'))
+    this.classList.add('active-featured')
+}
+LinkFeatured.forEach(l=> l.addEventListener('click', activeFeatured))
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
